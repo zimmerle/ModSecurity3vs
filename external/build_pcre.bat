49af37edae04fc1dff317ca433c7bb0925bbf30f
Felipe Zimmerle <felipe@zimmerle.org>
2019-04-03 15:12:17 -0300

    Adds scripts to build external deps: pcre, yajl

---
+++ external/build_pcre.bat
@@ -0,0 +1,4 @@
+
+cd pcre-8.43
+CMAKE -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=True
+nmake
---
+++ external/build_yajl.bat
@@ -0,0 +1,6 @@
+
+cd yajl
+mkdir build
+cd build
+cmake ..
+devenv YetAnotherJSONParser.sln /project ALL_BUILD /build Release
