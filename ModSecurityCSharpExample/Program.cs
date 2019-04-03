8deb6629a792189d88c1162876693a560bfa5a18
Felipe Zimmerle <felipe@zimmerle.org>
2019-04-03 19:28:40 -0300

    Adds ModSecurity CSHarpExemple skeleton

---
+++ ModSecurityCSharpExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandAndConquer.CLI.Core;
+
+namespace ModSecurityCSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor.ProcessArguments(args);
+        }
+    }
+}
